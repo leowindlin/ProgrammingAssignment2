8fffc16fe3f62f4cc4904dce9986fc2cd97bd7db
leowindlin <leowindlin@hotmail.com>
2015-08-17 17:54:29 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##First step: create makeCache function that
+##returns a "special matrix" that is a list
+##with functions to 1)set the value of a matrix,
+##2)get the value of a matrix, 3)set the value of 
+##the inverse and 4)get the value of the inverse
+makeCacheMatrix<- function(x = matrix()) 
+{
+  ##I will use m as the cache variable
+  m <- NULL
+  
+  ##I create the set function: it will set the
+  ##value of the matrix
+  set <- function(y) 
+  {
+    ##Set the y matrix to the x variable 
+    ##in the above enviroment
+    x <<- y 
+  
+    ##The cache must go back to NULL in 
+    ##the above enviroment
+    m <<- NULL
+  }
+  
+  ##I create the get function: it will return
+  ##the value of the matrix
+  get <- function() x
+  
+  ##We do the same for the inverse function
+  
+  ##Here I set the cache m as the inverse
+  ##of the matrix
+  setInverse <- function(inverse) m <<- inverse
+  
+  ##Here I return the cache
+  getInverse <- function() m
+  
+  #Return the results as a list
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##Second step: I create the function that
+##returns the inverse of a given matri.
+##The function checks if the inverse is
+##already calculated in the cache and
+##return it if so; otherwise, it
+##calculates the inverse
+cacheSolve <- function(x,...) {
+  
+  m <- x$getInverse()
+  if(!is.null(m))
+  {
+    print("Returning cache")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data) %*% data
+  x$setInverse(m)
+  m
 }
